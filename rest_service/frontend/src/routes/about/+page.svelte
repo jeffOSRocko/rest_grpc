63824ab6384193499c07e3e46ce4bc116cd28d25
Jeff Rockwell <jrockwel@gmail.com>
2024-01-15 15:16:34 -0500

    initial svelte

---
+++ rest_service/frontend/src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this app</h1>
+
+	<p>
+		This is a sample application that demonstrates a rest client talking to grpc client when
+		handling GET and POST requests
+	</p>
+</div>
